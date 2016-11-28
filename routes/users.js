9fd03286cbc22d6bba615f797dd8a6ade94c4a28
Hunter Leachman <leachmanh@MacBook-Air-2.local>
2016-11-28 15:13:18 -0600

    trying to convert raw64 to image

--- routes/users.js
+++ routes/users.js
@@ -6,11 +6,9 @@ var app = express();
 var multer  = require('multer')
 var upload = multer({ dest: 'uploads/' });
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-
 router.post('/add', function(req,res){
-	res.send("Hey, it's atleast getting to the route....")
- console.log(req.body)
+    res.send("Hey, it's atleast getting to the route....");
+    console.log(req.body)
 	});
 module.exports = router;
 
--- server.js
+++ server.js
@@ -8,43 +8,44 @@ var User = require('./models')['users'];
 const client = new skybiometry.Client(appKeys.key1, appKeys.key2);
 var bodyParser = require('body-parser');
 var app = express();
-app.use('/users', users);
-app.use('/', main);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-function startServer(){
+app.use('/users', users);
+app.use('/', main);
+function startServer() {
   app.listen(3000, function() {
-  User.sync(); // creates a users table
-  console.log('Listening on port 3000!');
-  });
- }
+    User.sync(); // creates a users table
+    console.log('Listening on port 3000!');
+    });
+  }
 function learnFace(name) {
     client.faces.detect({ urls: testVars.garryB, attributes: 'all' })
     .then(function(result){
         var nameString = String(name);
         var newLearn = JSON.parse(result);
         var newTid = newLearn.photos[0].tags[0].tid;
-        console.log(newTid);
         var fullNameSpace = name + '@peter';
         client.tags.save(newTid, fullNameSpace, {label: nameString, password: 'optionalPassword'});
         client.faces.train(nameString, { namespace: 'peter' });
         })
     };
 function recognizeFace(name) {
-    var fullNameSpace = name + '@peter';
-    client.faces.recognize(fullNameSpace, { urls: testVars.garryB, attributes: 'all' })
+  var fullNameSpace = name + '@peter';
+  client.faces.recognize(fullNameSpace, { urls: testVars.garryB, attributes: 'all' })
     .then(function(result) {
         var newData = JSON.parse(result);
         var user = newData.photos[0].tags[0];
-        console.log(user);
         var confidence = user.uids[0].confidence;
-        if (confidence > 40) {
+          if (confidence > 40) {
             console.log('This looks like a match. Were: ' + confidence + ' confident.');
-        } else {
+          } else {
             console.log('This is not a match. Were: ' + confidence + ' confident.');
-        }
-  });}
+          }
+      });
+  };
+
 startServer();
-learnFace('gary');
-recognizeFace('gary');
+learnFace();
+recognizeFace();
+
